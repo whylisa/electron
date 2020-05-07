460e6aa65ef88367db80e255381ad919caa28f56
haoyue.wang <843655240@qq.com>
2020-05-07 14:39:21 +0800

    filelist组件

--- cloud/src/App.js
+++ cloud/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import FileSearch from './components/FileSearch'
+import FileList from './components/FileList'
+import defaultFiles from './utils/defaultFiles'
 function App() {
   return (
     <div className="App container-fluid">
@@ -10,6 +12,12 @@ function App() {
              <FileSearch 
                title="点击云文档"
                onFileSearch={(value) => { console.log(value)}}
+             />
+             <FileList 
+               files={defaultFiles}
+               onFileClick={(id) => { console.log(id)}}
+               onFileDelete={(id) => { console.log('del',id)}}
+               onSaveEdit={(id, newValue) => {console.log(id, newValue)}}
              />
          </div>
          <div className="col-9 bg-primary right-panel">
