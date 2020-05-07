b1a31d3d113eb40789f96fd058e6d5be7d4b376a
haoyue.wang <843655240@qq.com>
2020-05-07 17:06:32 +0800

    按钮收尾

--- cloud/src/App.js
+++ cloud/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
+
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import FileSearch from './components/FileSearch'
 import FileList from './components/FileList'
 import defaultFiles from './utils/defaultFiles'
+import BottomBtn from "./components/BottomBtn";
 function App() {
   return (
     <div className="App container-fluid">
@@ -19,6 +22,22 @@ function App() {
                onFileDelete={(id) => { console.log('del',id)}}
                onSaveEdit={(id, newValue) => {console.log(id, newValue)}}
              />
+             <div className="row">
+               <div className="col-6">
+                  <BottomBtn 
+                    text="新建"
+                    colorClass="btn-primary"
+                    icon={faPlus}
+                  />
+               </div>
+               <div className="col-6">
+                   <BottomBtn 
+                    text="导入"
+                    colorClass="btn-success"
+                    icon={faFileImport}
+                  />
+               </div>
+             </div>
          </div>
          <div className="col-9 bg-primary right-panel">
 
